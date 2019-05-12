f1575447cf2bb71fd2c8ab595b9e6c732c7bddd5
belaifa_el_hussein <belaifahocine@gmail.com>
2019-05-12 11:49:46 +0200

    project summary component

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const projectSummary = () => {
+  return (
+    <div className="card z-depth-0 project-summary">
+      <div className="car-content grey-text text-darken-3">
+        <span className="card-title">Project Title</span>
+        <p>Posted by the Belaifa El Hussein</p>
+        <p className="grey-text">3rd September,2AM</p>
+      </div>
+    </div>
+  );
+};
+
+export default projectSummary;
