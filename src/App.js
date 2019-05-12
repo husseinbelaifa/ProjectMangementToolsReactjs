de2ed2970d52073e8b9045d12c598ff1b8f2640f
belaifa_el_hussein <belaifahocine@gmail.com>
2019-05-12 11:45:38 +0200

    dashboard component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
-import { BrowserRouter, route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
+import Dashboard from "./components/dashboard/Dashboard";
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
           <Navbar />
+          <Switch>
+            <Route path="/" component={Dashboard} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const ProjectList = () => {
+  return (
+    <div className="project-list section">
+      <div className="card z-depth-0 project-summary">
+        <div className="car-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by the Belaifa El Hussein</p>
+          <p className="grey-text">3rd September,2AM</p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <div className="car-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by the Belaifa El Hussein</p>
+          <p className="grey-text">3rd September,2AM</p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <div className="car-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by the Belaifa El Hussein</p>
+          <p className="grey-text">3rd September,2AM</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectList;
