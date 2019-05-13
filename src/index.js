1cd2452ac1fae1f47c3e14b72fefc66998f7d506
belaifa_el_hussein <belaifahocine@gmail.com>
2019-05-13 10:37:17 +0200

    config react redux with reducers

--- src/index.js
+++ src/index.js
@@ -6,12 +6,25 @@ import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./store/reducers/rootReducer";
 import thunk from "redux-thunk";
-
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+import fbConfig from "./config/fbConfig";
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers(
+    compose(
+      applyMiddleware(
+        thunk.withExtraArgument({
+          getFirebase,
+          getFirestore
+        })
+      ),
+      reduxFirestore(fbConfig),
+      reactReduxFirebase(fbConfig)
+    )
+  )
 );
 
 ReactDOM.render(
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,12 @@
-export const createProject = (project) => (dispatch, getState) => {
+export const createProject = (project) => (dispatch, getState, {
+ getFirebase,
+ getFirestore
+}) => {
 
  //make async call
+
+
+
  return dispatch({
   type: 'ADD_PROJECT',
   project: project
